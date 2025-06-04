5e6c0de130a79ca6fa8dc79e70aeeff800763985
Awointa <akosawointa@gmail.com>
2025-06-04 17:37:09 +0100

    set up test configurations

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ]
+};
